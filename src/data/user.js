6c0c79e6152a742d7e1cd721d3ad0a1c4b14c124
Stacy <stacykimilu@gmail.com>
2024-04-29 15:40:30 +0300

    react hooks props basics lab

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
-  city: "New York",
-  bio: "I made this!",
-  color: "firebrick",
+  name: "Stacy Kimilu",
+  city: "Nairobi",
+  bio: "I am passionate about Web Development and AI.",
+  color: "royalblue",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/stacykimilu",
+    linkedin: "https://www.linkedin.com/in/stacy-kimilu-018052207/",
   },
 };
 
